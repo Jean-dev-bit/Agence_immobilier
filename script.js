7c838b2ee1cf5e6b493e3cf4e078001b434b099e
Jean-dev-bit <jeanbaptistehouetchowanou@gmail.com>
2023-09-14 11:43:50 +0100

    Premier commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","DE COURTAGE IMMOBILIER "," DES BIENS IMMOBILIERS","DE VENTES DES BIENS","DE GESTION LOCATIVE"],
+    typedSpeed:100,
+    backSpeed:60,
+    loop:true
+})
